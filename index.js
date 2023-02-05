c66af3bcbbbbf59e5d559847b5a1d6346665a718
amyciriaco95 <amyciriaco95@gmail.com>
2023-02-05 07:17:45 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+//function expression
+
+const divide = function(x, y) {
+    return x / y;
+}
+
+//arrow function called square where the return value is the parameter (x) multiplied by itself//
+
+const square = x => x * x;
+
+//arrow function called add where the return value is the two parameters (x and y) are added together//
+
+const add = (x, y) => x + y;
+
